679381002d6be69d5e54c7a020dafeb1e6e6f596
Ankita Rajendra Alshi <aralshi@tank.soic.indiana.edu>
2017-11-14 23:29:34 -0500

    with comments

--- part2/ocr.py
+++ part2/ocr.py
@@ -7,6 +7,20 @@
 # (based on skeleton code by D. Crandall, Oct 2017)
 #
 # Please check the readme file for the description
+#
+# Approach: We first calculated the initial probabilities, the transition probabilities and emission probabilities based on our training dataset. These probabilities are then passed on to our algorithms (i.e.. Simplified, VE, and MAP).
+# Please note our approaches for Simplified, Variable Elimination and Viterbi ARE THE SAME for both programs: 
+# For the Simplified algorithm we calculate the max of P(E|S)P(S) we calculate P(S) by mainitaining a table that would contain the count of each label while training.
+# For the Variable Elimination algorithm we have used the forward-backward approach. Wherein alpha is P(St,E1,E2,E3...Et) and beta is P(St|Et+1,Et+2,....ET). 
+# We calculated the state where the product of alpha and beta is the maximum. We used dynamic programming to maintain tables for both.
+# For the Viterbi algorithm 
+# For question 1 we are using the following definitions of emissions and transitions: 
+# For question 2 we are using the following definitions of emissions and transitions:
+#        1. Initial: The probability of a charcter occuring as first character of the sentense. We are using Laplacian smoothing to get a better probability distribution and non-zero probability for characters not present in training data at 1st position.
+#        1. Emision: The probability of character occuring given an image. This is calculated by pixel matching of the matrix with the different matrices present in the training set. After experimentation and after findng better results, higher weight is assigned for matched pixels and less weight is assigned for matched spaces.
+#        2. Transition: The probability of a character occuring given the previous character. P('a'|'b') or P('b'|'c') and so on. We are using Laplacian smoothing for transitional probability as well to get a better probability distribution and non-zero probability for character transactions that were not present in training data.
+#
+# Assumptions: 1. Training data is a Text representing large number of English words. 2. Test image will always include characters from that training dataset. 
 
 from PIL import Image, ImageDraw, ImageFont
 import math
